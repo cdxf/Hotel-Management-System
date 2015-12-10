450fe51540f32288b66790b63a5602113dfb4467
quyenvinhxuan <doa9595@gmail.com>
2015-12-10 21:56:09 +0700

    delete npm

--- HMS/config/app.php
+++ HMS/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'viEImKRPKpIu61BuJju1hSkL5rluyiF5'),
 
     'cipher' => 'AES-256-CBC',
 
